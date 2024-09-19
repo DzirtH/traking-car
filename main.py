253f717dddef96d1880717d393c2ef174d4e1c57
DzirtH <68776022+DzirtH@users.noreply.github.com>
2024-09-19 11:11:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 import cv2
 from tracker import *
 
-# Create tracker object
+
 tracker = EuclideanDistTracker()
 
-cap = cv2.VideoCapture("Road - 84971.mp4")
+cap = cv2.VideoCapture("highway.mp4")
 
 # Object detection from Stable camera
-object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=50)
+object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
 
 while True:
     ret, frame = cap.read()
     height, width, _ = frame.shape
 
     # Extract Region of interest
-    roi = frame[580: 2000, 500: 1200]
+    roi = frame[340: 720,500: 800]
 
     # 1. Object Detection
     mask = object_detector.apply(roi)
-    _, mask = cv2.threshold(mask, 240, 250, cv2.THRESH_BINARY)
+    _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     detections = []
     for cnt in contours:
         # Calculate area and remove small elements
         area = cv2.contourArea(cnt)
-        if area > 350:
+        if area > 100:
             #cv2.drawContours(roi, [cnt], -1, (0, 255, 0), 2)
             x, y, w, h = cv2.boundingRect(cnt)
 
